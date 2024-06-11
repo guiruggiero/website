54dc6b7bc3be115c38e68303efc89ff5252a1eb9
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2024-06-11 23:56:02 +0000

    Basic Gemini API call working

--- guipt/tests/gemini.js
+++ guipt/tests/gemini.js
@@ -1,49 +1,44 @@
-/*
- * Install the Generative AI SDK
- *
- * $ npm install @google/generative-ai
- *
- * See the getting started guide for more information
- * https://ai.google.dev/gemini-api/docs/get-started/node
- */
+import { GEMINI_API_KEY } from "../../../secrets/guiruggiero.mjs";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import fs from "node:fs";
 
-// https://aistudio.google.com/app/prompts/1LX_TrePVoOAqZeIMJYNYGE7-lCQSTXAH
-
-const {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-  } = require("@google/generative-ai");
+// const {
+//     GoogleGenerativeAI,
+//     HarmCategory,
+//     HarmBlockThreshold,
+//   } = require("@google/generative-ai");
   
-  const apiKey = process.env.GEMINI_API_KEY; // Import from Secrets
-  const genAI = new GoogleGenerativeAI(apiKey);
-  
-  var fs = require('fs');
-  var data = fs.readFileSync('prompt.txt', 'utf8');
-  console.log(data);
+const apiKey = GEMINI_API_KEY;
+// console.log(apiKey);
+const genAI = new GoogleGenerativeAI(apiKey);
 
-  const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-pro",
-    systemInstruction: data ,
+// var fs = require("fs");
+var prompt = fs.readFileSync("../prompt.txt", "utf8");
+// console.log(prompt);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-pro",
+  // model: "gemini-1.5-flash",
+  systemInstruction: prompt,
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run() {
+  const chatSession = model.startChat({
+    generationConfig,
+  // safetySettings: Adjust safety settings
+  // See https://ai.google.dev/gemini-api/docs/safety-settings
   });
-  
-  const generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 64,
-    maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
-  };
-  
-  async function run() {
-    const chatSession = model.startChat({
-      generationConfig,
-   // safetySettings: Adjust safety settings
-   // See https://ai.google.dev/gemini-api/docs/safety-settings
-    });
-  
-    const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
-    console.log(result.response.text());
-  }
-  
-  run();
+
+  const result = await chatSession.sendMessage("What can you do?");
+  console.log(result.response.text());
+}
+
+run();
