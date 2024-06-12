fa00d172ca4af4c726a8352030fab4eec99d6de5
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2024-06-12 00:27:08 +0000

    Text-only prompt working

--- guipt/tests/gemini.js
+++ guipt/tests/gemini.js
@@ -1,5 +1,5 @@
 import { GEMINI_API_KEY } from "../../../secrets/guiruggiero.mjs";
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { ChatSession, GoogleGenerativeAI } from "@google/generative-ai";
 import fs from "node:fs";
 
 // const {
@@ -12,8 +12,7 @@ const apiKey = GEMINI_API_KEY;
 // console.log(apiKey);
 const genAI = new GoogleGenerativeAI(apiKey);
 
-// var fs = require("fs");
-var prompt = fs.readFileSync("../prompt.txt", "utf8");
+let prompt = fs.readFileSync("../prompt.txt", "utf8");
 // console.log(prompt);
 
 const model = genAI.getGenerativeModel({
@@ -30,15 +29,44 @@ const generationConfig = {
   responseMimeType: "text/plain",
 };
 
-async function run() {
-  const chatSession = model.startChat({
-    generationConfig,
-  // safetySettings: Adjust safety settings
-  // See https://ai.google.dev/gemini-api/docs/safety-settings
-  });
+const safetySettings = {
+  // https://ai.google.dev/gemini-api/docs/safety-settings
+};
 
-  const result = await chatSession.sendMessage("What can you do?");
-  console.log(result.response.text());
+async function text_input() {
+  const result = await model.generateContent("What can you do?");
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
 }
 
-run();
+// async function chat() { // TODO
+//   // The Gemini 1.5 models are versatile and work with multi-turn conversations (like chat)
+//   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+//   const chat = model.startChat({
+//     history: [
+//       {
+//         role: "user",
+//         parts: [{ text: "Hello, I have 2 dogs in my house." }],
+//       },
+//       {
+//         role: "model",
+//         parts: [{ text: "Great to meet you. What would you like to know?" }],
+//       },
+//     ],
+//     generationConfig: {
+//       maxOutputTokens: 100,
+//     },
+//   });
+
+//   const msg = "How many paws are in my house?";
+
+//   const result = await chat.sendMessage(msg);
+//   const response = await result.response;
+//   const text = response.text();
+//   console.log(text);
+// }
+
+text_input();
+// chat();
