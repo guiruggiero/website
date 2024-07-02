9ca4c63e2dc9d9a69b9ff5b99aac19c7f952dbe4
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2024-07-02 22:28:51 +0000

    Playing around with function

--- guipt/tests/functions/index.js
+++ guipt/tests/functions/index.js
@@ -1,5 +1,11 @@
 import * as functions from "firebase-functions";
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from GuiPT!");
+  let name = request.query.name;
+  if (!name) {
+    name = "undefined";
+  }
+
+  console.log("name passed is " + name);
+  response.send("Hello from GuiPT, " + name + "!");
 });
