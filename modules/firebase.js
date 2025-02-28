5d2d28a7f557f9489f8f380b4ae8931d65ca03d1
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2025-02-28 13:08:40 +0000

    Bump Firebase

--- modules/firebase.js
+++ modules/firebase.js
@@ -1,5 +1,5 @@
-import {getApp, getApps, initializeApp} from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
-import {getFirestore, addDoc, collection, doc, updateDoc} from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore-lite.js"
+import {getApp, getApps, initializeApp} from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import {getFirestore, addDoc, collection, doc, updateDoc} from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore-lite.js"
 
 // Initializations
 const firebaseConfig = {
