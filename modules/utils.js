8ee7ccb1b6eb35399a543904fbae3954fa433608
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2025-07-05 09:58:34 +0000

    Simplified getScriptPath

--- modules/utils.js
+++
@@ -1,9 +0,0 @@
-// modules/utils.js
-function getScriptPath(baseName) {
-  const isNgrok = window.location.href.includes("ngrok");
-  if (isNgrok) {
-    return `modules/${baseName}.js`;
-  } else {
-    return `modules/${baseName}.min.js`;
-  }
-}
